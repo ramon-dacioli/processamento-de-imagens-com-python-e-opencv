7f0eabf2a7ed2c4ee3a66c5bca32e211047e75b5
ramon-dacioli <ramon_muz@hotmail.com>
2025-07-07 10:59:29 -0300

    Rotacionando a imagem

---
+++ 19-exemplo.py
@@ -0,0 +1,9 @@
+import cv2
+
+imagem = cv2.imread('imagens/imagem.jpg')
+
+imagemrotacionada = imagem[::-1, ::-1]
+
+cv2.imshow("Imagem", imagem)
+cv2.imshow("Imagemrotacionada", imagemrotacionada)
+cv2.waitKey(0)
